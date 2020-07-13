6dbc49858781c3fc600123d3d683b2b3b0b7fb71
Yuichi Yogo <yuichkun@gmail.com>
2020-07-13 15:35:35 +0900

    add Environment switch

--- Startup.cs
+++ Startup.cs
@@ -15,19 +15,32 @@ namespace RazorPagesMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlite(
+                        Configuration.GetConnectionString("MovieContext")));
+            }
+            else
+            {
+                services.AddDbContext<RazorPagesMovieContext>(options =>
+                    options.UseSqlServer(
+                        Configuration.GetConnectionString("MovieContext")));
+            }
             services.AddRazorPages();
-            services.AddDbContext<RazorPagesMovieContext>(options => options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
-    }
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
